c5ccf93264cc8b3b40d757593e40a2f8c54bae83
Mitsuharu Emoto <mthr1982@gmail.com>
2026-05-04 23:08:12 +0900

    Revert "getBookEntries/getTocItemsのテスト追加、vitest設定修正、依存性注入対応"
    This reverts commit 68a894953a1c30ab474a323b1c4d9fe18239481e.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,20 +1,9 @@
-import { defineConfig, transformWithEsbuild } from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [
-    {
-      name: 'cts-transform',
-      async transform(code, id) {
-        if (id.endsWith('.cts')) {
-          return transformWithEsbuild(code, id, { loader: 'ts' })
-        }
-      },
-    },
-  ],
   test: {
     environment: 'node',
     globals: true,
     include: ['scripts/**/*.test.ts'],
-    execArgv: ['--require', 'tsx/cjs'],
   },
 })
